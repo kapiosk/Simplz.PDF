4acb18a38c62a59e447afebbb773a3a02c7483b8
AZ <speedsterk@hotmail.com>
2023-08-02 21:55:39 +0300

    init

---
+++ README.md
@@ -0,0 +1,77 @@
+# SimplzPDF
+
+## Installation
+
+```bash
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirement.txt
+python -m playwright install
+python -m playwright install-deps
+```
+
+## Deployment
+
+Service
+
+```bash
+sudo nano /etc/systemd/system/simplzpdf.service
+```
+
+```bash
+[Unit]
+Description=Gunicorn instance to serve simplzpdf
+After=network.target
+
+[Service]
+User=www-data
+Group=www-data
+WorkingDirectory=/var/www/html/SimplzPDF
+Environment="PATH=/var/www/html/SimplzPDF/venv/bin"
+ExecStart=/var/www/html/SimplzPDF/venv/bin/gunicorn --workers 3 --bind unix:simplzpdf.sock -m 007 wsgi:app
+
+[Install]
+WantedBy=multi-user.target
+```
+
+_Please note that if you are using globals and main user, you might need to ommit group and environment_
+
+Reload systemctl
+
+```bash
+sudo systemctl daemon-reload
+```
+
+Start service
+
+```bash
+sudo systemctl start simplzpdf.service
+```
+
+Check service status service
+
+```bash
+sudo systemctl status simplzpdf.service
+```
+
+Enable service if everything is ok
+
+```bash
+sudo systemctl enable simplzpdf.service
+```
+
+Nginx
+
+```bash
+server {
+    listen 80;
+    server_name simplzpdf.curis.health;
+        location / {
+        proxy_set_header Host $http_host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_pass http://unix:/var/www/html/SimplzPDF/simplzpdf.sock;
+    }
+}
+```
---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, Response
+from playwright.sync_api import sync_playwright
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
+def index():
+    return 'PostMe! PDF!!'
+
+@app.route('/Test', methods = ['GET'])
+def test():
+    with sync_playwright() as p:
+        with p.chromium.launch() as browser:
+            with browser.new_context(ignore_https_errors = True) as context:
+                page = context.new_page()
+                page.set_content('<p>Test</p>')
+                data = page.pdf(format = 'A4', print_background = True)
+                return Response(response = data, status = 200, mimetype = 'application/pdf')
+
+@app.route('/PDF', methods=['POST'])
+def pdf():
+    with sync_playwright() as p:
+        with p.chromium.launch() as browser:
+            with browser.new_context(ignore_https_errors = True) as context:
+                page = context.new_page()
+                page.set_content(request.get_data(as_text=True))
+                data = page.pdf(format = 'A4', print_background = True)
+                return Response(response = data, status = 200, mimetype = 'application/pdf')
+
+@app.route('/PDFURL', methods = ['GET'])
+def pdfFromURL():
+    dataUrl = request.args['dataUrl']
+    if dataUrl is not None:
+        with sync_playwright() as p:
+            with p.chromium.launch() as browser:
+                page = browser.new_page()
+                if 'Authorization' in request.headers:
+                    authorization = request.headers['Authorization']
+                    page.set_extra_http_headers({'Authorization': f'Bearer {authorization}'})
+                landscape = False
+                if 'landscape' in request.headers:
+                    landscape = True
+                page.goto(dataUrl)
+                page.wait_for_load_state('networkidle')
+                data = page.pdf(format = 'A4', print_background = True, landscape = landscape)
+                return Response(response = data, status = 200, mimetype = 'application/pdf')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5501)
